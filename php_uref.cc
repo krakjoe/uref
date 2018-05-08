3960e382287dde638cdf3f0c5caec276368d6e36
Joe Watkins <krakjoe@php.net>
2018-05-08 11:57:50 +0200

    ...

--- php_uref.cc
+++ php_uref.cc
@@ -52,10 +52,10 @@
 
 #if defined(__APPLE__)
 #	if defined(__LP64__)
-#		define UREF_IP_REG(mc) (mc)->__ss.rip
+#		define UREF_IP_REG(mc) (mc)->__ss.__rip
 		typedef uint64_t uref_register_t;
 #	else
-#		define UREF_IP_REG(mc) (mc)->__ss.eip
+#		define UREF_IP_REG(mc) (mc)->__ss.__eip
 		typedef unsigned int uref_register_t;
 #	endif
 #else
